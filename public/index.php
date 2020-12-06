c64df2ef76756df966a6e41a88887fe1f23154bb
ovod93 <alexander.shein1993@gmail.com>
2020-12-06 11:34:12 +0700

    настроена первая поисковая форма и обработчик

--- public/index.php
+++ public/index.php
@@ -7,34 +7,57 @@ use DI\Container;
 
 $container = new Container();
 $container->set('renderer', function () {
-    // Параметром передается базовая директория, в которой будут храниться шаблоны
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-});
+//$app->get('/', function ($request, $response) {
+//    $response->getBody()->write('Welcome to Slim!');
+//    return $response;
+//});
 
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
-});
+//$app->get('/users', function ($request, $response) {
+//    return $response->write('GET /users');
+//});
+//
+//$app->post('/users', function ($request, $response) {
+//    return $response->withStatus(302);
+//});
+//
+//$app->get('/courses/{id}', function ($request, $response, array $args) {
+//    $id = $args['id'];
+//    return $response->write("Course id: {$id}");
+//});
+//
+//$app->get('/users/{id}', function ($request, $response, $args) {
+//    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+//   return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+//});
 
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});
+$users = [
+    ['firstName' => 'mike'],
+    ['firstName' => 'mishel'],
+    ['firstName' => 'adel'],
+    ['firstName' => 'keks'],
+    ['firstName' => 'kamila'],
+];
 
-$app->get('/courses/{id}', function ($request, $response, array $args) {
-    $id = $args['id'];
-    return $response->write("Course id: {$id}");
-});
-
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
-    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+$app->get('/users', function(\Slim\Http\ServerRequest $request, $response) use ($users) {
+    $term = $request->getQueryParam('term');
+    $filteredUsers = [];
+    foreach ($users as $user) {
+        $pos = strpos($user['firstName'], $term);
+        if ($pos !== false) {
+            $filteredUsers[] = $user;
+        }
+    }
+//    var_dump($result);
+    $params = [
+        'filteredUsers' => $filteredUsers,
+        'term' => $term,
+        ];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
 $app->run();
