407d0aef6bbc90814b447f7d92b79f20b1a85d00
ovod93 <alexander.shein1993@gmail.com>
2020-12-10 18:08:02 +0700

    change routing

--- public/index.php
+++ public/index.php
@@ -12,65 +12,57 @@ $container->set('renderer', function () {
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-$users = [
-    ['firstName' => 'mike'],
-    ['firstName' => 'mishel'],
-    ['firstName' => 'adel'],
-    ['firstName' => 'keks'],
-    ['firstName' => 'kamila'],
-];
 
-//$app->get('/users', function(\Slim\Http\ServerRequest $request, $response) use ($users) {
-//    $term = $request->getQueryParam('term');
-//    $filteredUsers = [];
-//    foreach ($users as $user) {
-//        $pos = strpos($user['firstName'], $term);
-//        if ($pos !== false) {
-//            $filteredUsers[] = $user;
-//        }
-//    }
-////    var_dump($result);
-//    $params = [
-//        'filteredUsers' => $filteredUsers,
-//        'term' => $term,
-//        ];
-//    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-//});
-
-$app->get('/users/new', function ($request, $response) {
+$app->get('/users/autorization', function ($request, $response) {
     $params = [
-        'user' => ['name' => '', 'email' => '']
+        'user' => ['name' => '', 'email' => ''],
+        'errors' => [],
     ];
-    return $this->get('renderer')->render($response, "users/create_user.phtml", $params);
+    return $this->get('renderer')->render($response, "users/autorization.phtml", $params);
 });
 
 $app->post('/users', function ($request, $response) {
-    $user = $request->getParsedBodyParam('user');
-    $errors = validate($user);
-    if (count($errors) === 0) {
-        $repo->save($user);
-        return $response->withRedirect('/users', 302);
+        $user = $request->getParsedBodyParam('user');
+        $errors = validate($user);
+        if(cheсker($user) !== false) {
+            return $response->withRedirect('/users', 302);
+        }
+        $params = [
+            'user' => $user,
+            'errors' => $errors,
+        ];
+        return $this->get('renderer')->render($response, "users/autorization.phtml", $params);
+    });
+
+    function validate($user) {
+        $errors = [];
+        if(empty($user['name'])) {
+            $errors[] = 'Вы не ввели имя';
+        }
+        if(empty($user['email'])) {
+            $errors[] = 'Вы не ввели email';
+        }
+        return $errors;
+}
+
+    function cheсker($user) {
+        $rows = array_map('str_getcsv', file('public/dataBase.csv'));
+        $header = array_shift($rows);
+        $csv = [];
+        foreach($rows as $row) {
+            $csv[] = array_combine($header, $row);
+        }
+
+        foreach ($csv as $client) {
+            if($user['name'] !== $client['name'] || $user['email'] !== $client['email']) {
+                return false;
+                continue;
+            }
+            if ($user['name'] === $client['name'] && $user['email'] === $client['email']) {
+                return true;
+            }
+        }
     }
-    $params = [
-        'user' => $user,
-        'errors' => $errors
-    ];
-    return $this->get('renderer')->render($response, "users/new.phtml", $params);
-});
 
 $app->run();
 
-function validate($user) {
-    $errors = [];
-    if(empty($user['name'])) {
-        $errors[] = 'Вы не ввели имя';
-    }
-    if(empty($user['email'])) {
-        $errors[] = 'Вы не ввели email';
-    }
-    return $errors;
-}
-
-function chek($user) {
-
-}
